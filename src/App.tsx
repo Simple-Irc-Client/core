995a4261ebaa9498f09fcc91380d1b2397f42a93
Piotr Luczko <piotr.luczko@gmail.com>
2023-02-12 02:58:52 +0100

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ function App() {
           >
             <Stack direction="row">
               <Channels />
-              <Stack direction="column">
+              <Stack direction="column" minWidth="calc(100% - 400px)">
                 <Topic />
                 <Main />
                 <Toolbar />
