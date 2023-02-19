285060ad520cb5ad10a7db11151692464df94ae7
Piotr Luczko <piotr.luczko@gmail.com>
2023-02-19 14:48:54 +0100

    fix connect

--- src/pages/creator/CreatorLoading.tsx
+++ src/pages/creator/CreatorLoading.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Box, LinearProgress } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { ircConnect } from '../../network/network';
@@ -7,7 +7,7 @@ import { useSettingsStore } from '../../store/settings';
 const CreatorLoading = (): JSX.Element => {
   const { t } = useTranslation();
 
-  const [progress, setProgress] = useState({ value: 1, label: '' });
+  const [progress, setProgress] = useState({ value: 0, label: '' });
 
   const nick = useSettingsStore((state) => state.nick);
   const server = useSettingsStore((state) => state.server);
@@ -16,16 +16,24 @@ const CreatorLoading = (): JSX.Element => {
   const isConnected = useSettingsStore((state) => state.isConnected);
   const setCreatorStep = useSettingsStore((state) => state.setCreatorStep);
 
+  const ircConnectRequested = useRef(false);
+
   useEffect(() => {
+    if (server !== undefined && !isConnecting && !isConnected && !ircConnectRequested.current) {
+      ircConnectRequested.current = true;
+      console.log('sending connect to irc command');
+      ircConnect(server, nick);
+      setIsConnecting(true);
+    }
     if (isConnecting) {
-      setProgress({ value: 30, label: t('creator.loading.connecting') });
+      setProgress({ value: 1, label: t('creator.loading.connecting') });
     }
     if (isConnected) {
-      setProgress({ value: 50, label: t('creator.loading.connected') });
+      setProgress({ value: 2, label: t('creator.loading.connected') });
 
       setTimeout(() => {
         setProgress({
-          value: 50,
+          value: 3,
           label: t('creator.loading.isPasswordRequired'),
         });
       }, 2_000); // 2 sec
@@ -33,25 +41,16 @@ const CreatorLoading = (): JSX.Element => {
       setTimeout(() => {
         const localSettings = useSettingsStore.getState();
         if (localSettings.isPasswordRequired === false || localSettings.isPasswordRequired === undefined) {
-          setProgress({
-            value: 50,
-            label: t('creator.loading.passwordIsNotRequired'),
-          });
           setCreatorStep('channels');
         }
       }, 5_000); // 5 sec
-    }
-    if (server != null && !isConnecting && !isConnected) {
-      console.log('sending connect to irc command');
-      ircConnect(server, nick);
-      setIsConnecting(true);
     }
   }, [isConnecting, isConnected]);
 
   return (
     <>
       <Box sx={{ width: '100%', mt: 3 }}>
-        <LinearProgress variant="determinate" value={progress.value} />
+        <LinearProgress variant="determinate" value={progress.value * 30} />
         {progress.label !== '' && <h2 className="tw-text-center tw-mt-4">{progress.label}</h2>}
       </Box>
     </>
