824659328531314d7c15183e668355df4028d9a8
Piotr Luczko <piotr.luczko@gmail.com>
2023-02-28 01:43:04 +0100

    fix topic refresh

--- src/containers/Topic.tsx
+++ src/containers/Topic.tsx
@@ -5,11 +5,10 @@ import { useSettingsStore } from '../store/settings';
 
 const Topic = (): JSX.Element => {
   const currentChannelName: string = useSettingsStore((state) => state.currentChannelName);
-  const getTopic = useChannelsStore((state) => state.getTopic);
 
   return (
     <Box sx={{ paddingLeft: '16px', paddingRight: '16px', display: 'flex' }}>
-      <Input value={getTopic(currentChannelName)} disabled sx={{ marginBottom: '1rem', flexGrow: '1', minHeight: '48px' }} />
+      <Input value={useChannelsStore().getTopic(currentChannelName)} disabled sx={{ marginBottom: '1rem', flexGrow: '1', minHeight: '48px' }} />
     </Box>
   );
 };
