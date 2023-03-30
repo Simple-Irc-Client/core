d09fd41715263f23a829f3ac2f10990037d6e7dd
Piotr Luczko <piotr.luczko@gmail.com>
2023-03-31 00:43:39 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    hmr: false,
-  },
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      plugins: [react()],
+      server: {
+        hmr: false,
+      },
+    }
+  } else {
+    return {
+      plugins: [react()],
+      server: {
+        hmr: false,
+      },
+      base: './'
+    }
+  }
 });
