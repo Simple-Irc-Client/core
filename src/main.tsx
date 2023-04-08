e163e74e74470fc700cabc1231c172a2b5341eff
Piotr Luczko <piotr.luczko@gmail.com>
2023-04-08 02:18:32 +0200

    sentry

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn: 'https://d278b7ea51bb4875a83db57b6771a050@o281310.ingest.sentry.io/4504975631712256',
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
